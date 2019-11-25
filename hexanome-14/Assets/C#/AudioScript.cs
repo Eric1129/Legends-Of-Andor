797ef16a19fcc46b6a3c94e2f2d2eeb6987ade03
Chelsea Wright <chelseaw@Chelseas-MacBook-Pro.local>
2019-11-25 13:01:01 -0500

    I added music (hard metal) to the Play_Game screen, to activate, press play and the space bar)

---
+++ hexanome-14/Assets/C#/AudioScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MusicSource.clip = MusicClip;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MusicSource.Play();
+    }
+}
