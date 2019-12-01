58dd57b245ed8ef78f0961ea6da398d806463ad5
Brendonk13 <brendonk13.bk@gmail.com>
2019-12-01 13:59:32 -0500

    added brendons files

---
+++ hexanome-14/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private int heroNumber;
+    private string playerID;
+    private bool isMyTurn;
+
+
+    public void setParameters(int num, string ID, bool true_or_false)
+    {
+        heroNumber = num;
+        playerID = ID;
+        isMyTurn = true_or_false;
+    }
+
+
+    void Update()
+    {
+        // ie left-click
+        if (Input.GetMouseButtonDown(0))
+        {
+            masterClass master = gameObject.GetComponentInParent<masterClass>();
+            master.notifyClick();
+        }
+    }
+
+}
