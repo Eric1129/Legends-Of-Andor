6a6a09289157d36c2c20fb5f9dbb41a750d54186
anahita-m <43353141+anahita-m@users.noreply.github.com>
2020-04-20 21:53:09 -0500

    Create MedicinalHerb.cs

---
+++ hexanome-14/Assets/In_Game/C#/Game/MedicinalHerb.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicinalHerb : MonoBehaviour
+{
+
+	private Node location;
+	public bool used;
+	protected GameObject prefab;
+
+	public MedicinalHerb(Node startingPos, GameObject prefab)
+	{
+		location = startingPos;
+		this.prefab = prefab;
+		used = false;
+	}
+
+	private MedicinalHerb() { }
+
+	public int getLocation()
+	{
+		return location.getIndex();
+	}
+
+	public void setLocationNode(Node x)
+	{
+		location = x;
+	}
+
+	public Node getLocationNode()
+	{
+		return location;
+	}
+
+	public GameObject getPrefab()
+	{
+		return prefab;
+	}
+
+}
